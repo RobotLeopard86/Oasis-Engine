149d4ad7449232fb3d7519c9dd286d2768f626e4
RobotLeopard86 <63123751+RobotLeopard86@users.noreply.github.com>
2023-01-08 09:48:04 -0800

    Changed cube to be solid

--- Oasis-Engine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Oasis-Engine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -11,7 +11,8 @@ namespace Oasis {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 	void OpenGLRendererAPI::DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray) {
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
 		glDrawElements(GL_TRIANGLES, vertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
 	}
 }
